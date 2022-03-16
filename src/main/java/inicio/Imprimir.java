068db30aeae0b23e394b08dc2cdb6c6aa3bcbdeb
unknown <msp.paucar@gmail.com>
2022-03-15 23:15:54 -0500

    primer paso

---
+++ src/main/java/inicio/Imprimir.java
@@ -0,0 +1,15 @@
+package inicio;
+
+public class Imprimir {
+
+	public static void main(String[] args) {
+		System.out.print("Nombre: ");
+		System.out.println("Marco");
+		System.out.print("Apellido: ");
+		System.out.println("Paucar");
+		System.out.print("Pais: ");
+		System.out.println("Ecuador");
+
+	}
+
+}
